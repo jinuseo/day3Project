1f5b56390e182c6441c1906303d706c50e869df1
jinu <jinufilm@gmail.com>
2023-09-13 16:21:08 +0900

    뭐야

--- loop/whileSample.py
+++ loop/whileSample.py
@@ -21,5 +21,46 @@ def test_while2():
         if num == 0:
             break
 
+#반복 횟수가 정해지지 않은 경우 while 문을 주로 사용함
+#문자 하나를 입력받아서, 그 문자의 유니코드를 출력하는 처리를 반복
+#단, 0이 입력되면 반복이 종료됨
+def print_unicode():
+    ch = input('문자 하나를 입력 [0 입력시 종료됨] :')
+
+    while ch != '0':
+        print(f'{ch} is unicode {ord(ch)}')
+        ch = input('문자 하나를 입력 [0 입력시 종료됨] :')
+
+def print_unicode2():
+    while True:
+        ch = input('문자 하나를 입력 [0 입력시 종료됨] :')
+        if ch =='0':
+            break
+        print(f'{ch} is unicode {ord(ch)}')
+
+#파이썬에서는 여러 줄의 문자열을 표현할 때 3쌍의 따옴표를 이용할 수 있음
+def display_menu():
+    prompt='''
+    ※ 원하는 메뉴 번호를 선택하세요. ※
+    
+    1. 추가
+    2. 삭제
+    3. 출력
+    4. 끝내기
+    '''
+    #print(prompt)
+    while True:
+        print(prompt)
+        no = int(input('번호 입력 : '))
+
+        if no == 4:
+            break
+    print('======= end =======')
+
+#함수 실행
 if __name__ == '__main__':
-    test_while()
+    #test_while()
+    #test_while2()
+    #print_unicode()
+    #print_unicode2()
+    display_menu()
